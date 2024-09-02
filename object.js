7935164b8f770af5d98d3066ab28d0ab81c946c1
MistyIslam5 <mdmisty5@gmail.com>
2024-09-02 22:51:53 +0600

    object learning

---
+++ object.js
@@ -0,0 +1,18 @@
+const bottle = [
+    {
+        name: 'Aquarius',
+        description: 'The Aquarius is the most intelligent and wise of all signs.',
+        image: 'aquarius.jpg',
+        color: 'blue',
+        zodiacSign: 'Water'
+    },
+    {
+        name: 'Pisces',
+        description: 'The Pisces is the most beautiful and mysterious of all signs.',
+        image: 'pisces.jpg',
+        color: 'pink',
+        zodiacSign: 'Water'
+    }
+]
+
+console.log(bottle[0].name);
---
+++ revers.js
@@ -0,0 +1,10 @@
+let myName = 'Mizanur Rahman Misty ahmed Islam';
+
+let reverse = '';
+
+for(let Name of myName){
+    // console.log(Name);
+    reverse = Name + reverse ;
+}
+console.log(reverse);
+
